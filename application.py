8c1fbb61667ae6f5e1229d95d94feb17782a0551
Sarah Albert <sgalbert818@yahoo.com>
2024-11-05 17:06:25 -0700

    env

--- application.py
+++ application.py
@@ -26,7 +26,7 @@ application.config.update(
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = 'http://localhost:5000/callback'
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
